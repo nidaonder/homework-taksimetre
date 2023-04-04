dc710bd35b83a2c1b6c7bfe427b2a4e1f56874bc
nida önder <nidaaonder@gmail.com>
2023-04-05 00:43:54 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int km ;
+        double perKm = 2.20 , total = 10 ;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print(" KM giriniz: ");
+        km = input.nextInt() ;
+
+        total += (km * perKm) ;
+
+        total = (total <20) ? 20 : total ;
+        System.out.print(total + "TL");
+
+    }
+}
